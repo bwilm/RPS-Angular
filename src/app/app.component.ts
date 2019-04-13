ffce452ef4a87bc8eb1c2be65c06c1810539c3d0
bwilm <32461222+bwilm@users.noreply.github.com>
2019-04-13 10:22:23 -0400

    rps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import {
+  Component
+} from '@angular/core';
+import { tick } from '@angular/core/src/render3/instructions';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,75 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'RPS';
+
+  deImage: string;
+  deMessage: string;
+  userInput: '';
+
+clearFields(){
+  this.deImage = null;
+  this.deMessage = null
+  }
+
+
+  onSubmit(userInput)
+  {
+   // this.clearFields()
+
+    try{
+    this.deMessage =   this.getAiResponse(userInput).display
+    this.deImage =  this.getAiResponse(userInput).gameImage
+
+    }
+    catch(error){
+      console.log(error)
+    }
+
+  }
+
+
+  getAiResponse = function getAiResponse(userInput) {
+
+    let gameElements = ['rock', 'paper', 'scissors']
+
+    let displayElements = {
+      gameImage: 'string',
+      display: 'string'
+    }
+
+    let gameChoice = gameElements[Math.floor(Math.random() * gameElements.length)]
+
+    switch (gameChoice) {
+
+      case 'rock':
+      {
+        displayElements.gameImage = '../assets/images/rock.jpg';
+
+        (gameChoice === userInput ? displayElements.display = 'draw!': userInput === 'paper' ? displayElements.display = 'You Win!' : displayElements.display = "You Lose!")
+            break;
+        }
+
+      case 'paper':
+      {
+        displayElements.gameImage = '../assets/images/paper.png';
+
+        (gameChoice === userInput ? displayElements.display = 'draw!': userInput === 'rock' ? displayElements.display = 'You Lose!' : displayElements.display = "You Win!")
+        break;
+      }
+
+      case 'scissors':
+      {
+        displayElements.gameImage = '../assets/images/scissors.jpg';
+
+        (gameChoice === userInput ? displayElements.display = 'draw!': userInput === 'rock' ? displayElements.display = 'You Win!' : displayElements.display = "You Lose!")
+
+      break;
+      }
+    }
+    return displayElements;
+  };
+
+
+
+
 }
